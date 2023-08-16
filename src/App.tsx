41184d90610d83c546d016b83af519396e9d7686
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-08-16 20:30:45 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useMemo } from "react"
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from "./theme"
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
-    <>
-    </>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
   )
 }
 
